a5461803575ae4faaedd0201923e1dacef67c99a
codletech <codletech@gmail.com>
2014-02-27 08:34:39 +0200

    Update pages_manager.js

--- NetivotiApp/www/utils/pages_manager.js
+++ NetivotiApp/www/utils/pages_manager.js
@@ -229,7 +229,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 //
 function onDeviceReady() {
     // Register the event listener
-    document.addEventListener("backbutton", cPages.globalMoveBack, false);
+    document.addEventListener("backbutton", cPages.globalMoveBack, true);
 }
 
 
