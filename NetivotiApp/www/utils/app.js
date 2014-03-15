4d3969b0045b5497754c67c8e9371cfcec2d05d4
tallevi12 <tallevi12@gmail.com>
2014-03-15 23:45:10 +0200

    pushwoosh

--- NetivotiApp/www/utils/app.js
+++ NetivotiApp/www/utils/app.js
@@ -2,43 +2,111 @@
  * App object.
  */
 var app;
+
 app = {
     container: document.getElementById("app_container"),
     init: function() {
         FastClick.attach(document.body);
-        document.addEventListener("deviceready", initPushwoosh, true);
         cPages.moveToPage(this.container,"main","right");
     }
 
 
 };
 
-app.init();
+var pushNotification;
 
-function initPushwoosh()
+// handle APNS notifications for iOS
+function onNotificationAPN(e)
 {
-    alert("Tal Levi and Dvir Cohen");
-    var pushNotification = window.plugins.pushNotification;
-    pushNotification.onDeviceReady();
+    if (e.alert)
+    {
+        navigator.notification.alert(e.alert);
+    }
 
-    pushNotification.registerDevice({ projectid: "centered-sol-520", appid : "94550-3136B" },
-        function(status) {
-            var pushToken = status;
-            console.warn('push token: ' + pushToken);
-        },
-        function(status) {
-            console.warn(JSON.stringify(['failed to register ', status]));
-        }
-    );
+    if (e.sound)
+    {
+        var snd = new Media(e.sound);
+        snd.play();
+    }
 
-    document.addEventListener('push-notification', function(event) {
-        var title = event.notification.title;
-        var userData = event.notification.userdata;
+    if (e.badge)
+    {
+        pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+    }
 
-        if(typeof(userData) != "undefined") {
-            console.warn('user data: ' + JSON.stringify(userData));
-        }
-
-        navigator.notification.alert(title);
-    });
 }
+
+// handle GCM notifications for Android
+function onNotificationGCM(e)
+{
+    switch( e.event )
+    {
+        case 'registered':
+            if ( e.regid.length > 0 )
+            {
+                // Your GCM push server needs to know the regID before it can push to this device
+                // here is where you might want to send it the regID for later use.
+                PushWoosh.appCode = "94550-3136B";
+                PushWoosh.register(e.regid, function(data)
+                {
+                    console.log("PushWoosh register success: " + JSON.stringify(data));
+                }, function(errorregistration) {
+                    console.log("Couldn't register with PushWoosh" +  errorregistration);
+                });
+
+            }
+            break;
+
+        case 'message':
+            // if this flag is set, this notification happened while we were in the foreground.
+            // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+            if (e.foreground)
+            {
+                // if the notification contains a soundname, play it.
+                var my_media = new Media("/android_asset/www/"+e.soundname);
+                my_media.play();
+            }
+
+            break;
+
+        default:
+            break;
+    }
+}
+
+try
+{
+    pushNotification = window.plugins.pushNotification;
+    if (device.platform == 'android' || device.platform == 'Android')
+    {
+        pushNotification.register(successHandler, errorHandler, {"senderID":"centered-sol-520","ecb":"onNotificationGCM"});		// required!
+    }
+    else
+    {
+        pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});	// required!
+    }
+}
+catch(err)
+{
+    txt="There was an error on this page.\n\n";
+    txt+="Error description: " + err.message + "\n\n";
+    //alert(txt);
+}
+
+function tokenHandler (result) {
+    //$("#app-status-ul").append('<li>token: '+ result +'</li>');
+    // Your iOS push server needs to know the token before it can push to this device
+    // here is where you might want to send it the token for later use.
+}
+
+function successHandler (result) {
+    //$("#app-status-ul").append('<li>success:'+ result +'</li>');
+}
+
+function errorHandler (error) {
+    //$("#app-status-ul").append('<li>error:'+ error +'</li>');
+}
+
+
+
+app.init();
